69ac0427fa1086ec512d964c0748a17da898c568
alexander duque <alexduque541@gmail.com>
2023-03-29 12:01:43 -0400

    pagina creada

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+document.getElementById("compra_confirmar").onclick = function () {
+
+
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Formulario enviado Con Exito',
+        showConfirmButton: false,
+        timer: 2500
+    })
+}
